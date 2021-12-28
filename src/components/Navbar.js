899f8668afa3216ce2d2c0939249fb82c17e957b
hitesh <hitesh22rana@gmail.com>
2021-12-28 22:32:39 +0530

    UI/UX is improved

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,32 @@
-import React, { Component } from 'react'
 import './Navbar.css'
 import logo from '../logo.png';
 import { Link } from "react-router-dom";
+import * as FaIcons from 'react-icons/fa';
+import React from 'react'
+import { useState } from 'react';
 
-export class Navbar extends Component {
+function Navbar({setSidebar , sidebar}) { 
+    
+    const [icon, seticon] = useState(false);
 
-    render() {
-        return (
-            <section id="nav-bar-container">
-                <nav className="nav-bar">
-                    <div className="left-nav-bar">
-                        <img id="logo" src={logo} alt="logo" />
-                        <Link to="/">CyberPecker</Link>
-                    </div>
-                    <div className="right-nav-bar">
-                        <Link to="/">Home</Link>
-                        <Link to="/">About</Link>
-                    </div>
-                </nav>
-            </section>
-        )
+    const showSidebar = () => {
+        setSidebar(!sidebar);
+        seticon(!icon);
     }
+
+    return (
+        <section id="nav-bar-container">
+            <nav className="nav-bar">
+                <div className="left-nav-bar">
+                    <img id="logo" src={logo} alt="logo" />
+                    <Link to="/">CyberPecker</Link>
+                </div>
+                <div className="right-nav-bar"> 
+                    {icon === true ? <FaIcons.FaTimes onClick={showSidebar} /> : <FaIcons.FaBars onClick={showSidebar} />}
+                </div>
+            </nav>
+        </section>
+    )
 }
 
 export default Navbar
