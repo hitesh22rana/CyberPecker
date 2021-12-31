4bfd69af1f1301eb91f9df7afae3ad7d7a891293
hitesh <hitesh22rana@gmail.com>
2022-01-01 01:03:15 +0530

    dark mode is added

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './NewsItem.css'
+import '../colorToggle.css';
 
-export class NewsItem extends Component {
+function NewsItem(props) {
 
-    render() {
-        // Extracting information using props
-        let {title , author , description , imgURL , newsURL} = this.props;
+    // Extracting information using props
+    let {title , author , description , imgURL , newsURL} = props;
 
-        let colors = ['#e5989b','#d62828','#ef476f','#e76f51'];
+    let colors = ['#e5989b','#d62828','#ef476f','#e76f51'];
 
-        return (
+    return(
+
             <div className="news-card">
                 <div className="style-color" style={{backgroundColor : colors[Math.floor(Math.random() * colors.length)]}}></div>
                 <div className="news-img-container">
@@ -22,8 +23,8 @@ export class NewsItem extends Component {
                     <a rel="noreferrer" href={newsURL} target="_blank" id="read-more-button">Read More</a>
                 </div>
             </div>
-        )
-    }
+        
+    )
 }
 
 export default NewsItem
