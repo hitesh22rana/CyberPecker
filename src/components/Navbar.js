dd668de63163dd5fabd2b476001c4ef72db7029c
hitesh <hitesh22rana@gmail.com>
2022-01-01 01:02:03 +0530

    dark mode is added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,9 @@ import { Link } from "react-router-dom";
 import * as FaIcons from 'react-icons/fa';
 import React from 'react'
 import { useState } from 'react';
+import '../colorToggle.css';
 
-function Navbar({setSidebar , sidebar}) { 
+function Navbar({setSidebar , sidebar , setBgMode , bgmode}) { 
     
     const [icon, seticon] = useState(false);
 
@@ -14,14 +15,23 @@ function Navbar({setSidebar , sidebar}) {
         seticon(!icon);
     }
 
+    const toggleBgMode = () => {
+        setBgMode(!bgmode);
+    }
+
     return (
-        <section id="nav-bar-container">
+        <section id="nav-bar-container" className={bgmode === true ? 'light-mode-navbar' : 'dark-mode-navbar'}>
             <nav className="nav-bar">
                 <div className="left-nav-bar">
                     <img id="logo" src={logo} alt="logo" />
                     <Link to="/">CyberPecker</Link>
                 </div>
+
                 <div className="right-nav-bar"> 
+                    <label className="switch">
+                        <input type="checkbox" onClick={toggleBgMode}/>
+                        <span className="slider round"></span>
+                    </label>
                     {icon === true ? <FaIcons.FaTimes onClick={showSidebar} /> : <FaIcons.FaBars onClick={showSidebar} />}
                 </div>
             </nav>
