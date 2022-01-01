d76a65427df59f7216fb404ca3e615ca5040c20d
hitesh <hitesh22rana@gmail.com>
2022-01-01 15:28:15 +0530

    color mode fixed

---
+++ src/components/Headline.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Headline.css';
+
+function Headline({bgmode}) {
+    return (
+        <div className='headline-container'>
+            <h1 className='headline-heading' style={bgmode === true ? {color : 'rgba(15,15,14,0.8'} : {color : 'rgba(245, 245, 245, 0.8)'}}>Headlines</h1>
+        </div>
+    )
+}
+
+export default Headline
