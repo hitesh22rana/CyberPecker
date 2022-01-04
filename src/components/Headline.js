c7fff28ce3418507d5a8db69f05eb0764ae256fc
hitesh <hitesh22rana@gmail.com>
2022-01-04 16:02:09 +0530

    News Date is added and Card UI/UX is Updated

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -4,7 +4,7 @@ import './Headline.css';
 function Headline({bgmode}) {
     return (
         <div className='headline-container'>
-            <h1 className='headline-heading' style={bgmode === true ? {color : 'rgba(15,15,14,0.8'} : {color : 'rgba(245, 245, 245, 0.8)'}}>Headlines</h1>
+            <h1 className='headline-heading' style={bgmode === true ? {color : 'rgba(15,15,14,0.8'} : {color : 'rgba(245, 245, 245, 1)'}}>Headlines</h1>
         </div>
     )
 }
