e96d4646e946ce9bff67845540c48f900c5a0a68
hitesh <hitesh22rana@gmail.com>
2022-06-12 00:37:40 +0530

    New API is added and endpoint is changed and performance is increased

--- src/components/News.js
+++ src/components/News.js
@@ -14,7 +14,7 @@ function News({ category }) {
 
     useEffect(() => {
         const fetchNews = async () => {
-            const url = `https://cyber-news-api.herokuapp.com/${category}`;
+            const url = `https://jsln0v.deta.dev/news/${category}`;
             setLoading(true);
             let data = await fetch(url);
             let parsedData = await data.json();
@@ -31,9 +31,16 @@ function News({ category }) {
             <section id="news-section">
                 <div className="news-container">
                 {articles?.map((element) => {
-                    return <div className="news" key={element.id}>
-                        <NewsItem title = {element.headlines?element.headlines:""} author = {element.author?element.author:"Unknown"} description = {element.fullNews.length > 200 ? element.fullNews.slice(0 , 200) + '...' : element.fullNews} imgURL = {element.imgToURL?element.imgToURL:logo} newsURL = {element.newsUrl} newsDate = {element.newsDate}/>
-                    </div>
+                    return (
+                        <div className="news" key={element.id}>
+                            <NewsItem 
+                                title = {element.headlines?element.headlines:""} 
+                                author = {element.author?element.author:"Unknown"} 
+                                description = {element.fullNews.length > 200 ? element.fullNews.slice(0 , 200) + '...' : element.fullNews} 
+                                imgURL = {element.newsImgURL?element.newsImgURL : logo} 
+                                newsURL = {element.newsUrl} newsDate = {element.newsDate}/>
+                        </div>
+                    )
                 })}
                 </div>
             </section>
